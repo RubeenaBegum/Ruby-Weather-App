7347587741b224e7c4f15f9e6e2757348f938c36
RubeenaBegum <rubeenabegum13797@gmail.com>
2021-11-25 00:51:45 +0530

    First commit

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -13,7 +11,7 @@ module.exports = function (config) {
       require('karma-junit-reporter')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     reporters: ['mocha', 'junit'],
     port: 9876,
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Weather Details';
 
   weatherData = [
     {
